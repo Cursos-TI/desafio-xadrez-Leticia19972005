05f25022e775af77a2307122b5514fd35cc6a46c
Leticia19972005 <leticiabarros2005.2.2@gmail.com>
2025-08-11 22:44:35 +0000

    Movimentação cavalo

--- xadrez.c
+++ xadrez.c
@@ -3,6 +3,7 @@
 int main() {
     int t = 1;
     int b = 1;
+    int e = 1;
     int r;
 
     printf("Torre:\n");
@@ -11,17 +12,32 @@ int main() {
         printf("Direita\n");
         t++;
     }
+
+    printf("\n");
     printf("Bispo:\n");
     do
     {
         printf("Cima,Direita\n");
         b++;
     } while (b<=5);
+
+    printf("\n");
     printf("Rainha:\n");
     for (r = 1; r <= 8; r++)
     {
         printf("Esquerda\n");
     }
-    
+    printf("\n");
+    printf("Cavalo:\n");
+    for (int c = 1; c < 2; c++)
+    {
+        while (e<=2)
+        {
+            printf("Baixo\n");
+            e++;
+        }
+        
+        printf("Esquerda\n");
+    }
     return 0;
 }
